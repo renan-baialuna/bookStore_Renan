25c601a7e726d81cd928a123a595e0b028df64cd
Renan <renan.baialuna@gmail.com>
2021-01-19 18:10:40 -0300

    Add correct size for cells

--- BookStore_Renan/ViewControllers/BooksCollectionViewController.swift
+++ BookStore_Renan/ViewControllers/BooksCollectionViewController.swift
@@ -9,15 +9,22 @@ import UIKit
 
 
 class BooksCollectionViewController: UIViewController {
-    var books = [1, 2, 3, 4]
+    var books = [1, 2, 3, 4, 5, 6]
     @IBOutlet weak var booksCollection: UICollectionView!
+    @IBOutlet weak var flowLayout: UICollectionViewFlowLayout!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.booksCollection.delegate = self
         self.booksCollection.dataSource = self
         // Do any additional setup after loading the view.
-        booksCollection.reloadData()
+        
+        let space:CGFloat = 5.0
+        let dimension = ((view.frame.size.width - 20) - (2 * space)) / 2
+
+        flowLayout.minimumInteritemSpacing = space
+        flowLayout.minimumLineSpacing = space
+        flowLayout.itemSize = CGSize(width: dimension, height: dimension * 5 / 3)
     }
     
 
