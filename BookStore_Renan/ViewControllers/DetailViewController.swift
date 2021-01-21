23b76e4e65f490b223d619e1eb181ffc1f468b39
Renan <renan.baialuna@gmail.com>
2021-01-21 00:18:27 -0300

    Add shortcut to open safary

--- BookStore_Renan/ViewControllers/DetailViewController.swift
+++ BookStore_Renan/ViewControllers/DetailViewController.swift
@@ -14,10 +14,18 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var titleLable: UILabel!
     @IBOutlet weak var bookImage: UIImageView!
     @IBOutlet weak var descriptionBookTextField: UITextView!
+    @IBOutlet weak var buyButton: UIButton!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        buyButton.layer.cornerRadius = 10
+        buyButton.layer.masksToBounds = true
+        
+        if book.volume.saleInfo.buyLink == nil {
+            buyButton.isHidden = true
+        }
         
         self.title = book.volume.volumeInfo.title
         // Do any additional setup after loading the view.
@@ -30,15 +38,12 @@ class DetailViewController: UIViewController {
         descriptionBookTextField.text = book.volume.volumeInfo.description
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func buyAction() {
+        if let urlString = book.volume.saleInfo.buyLink {
+            if let url = URL(string: urlString) {
+                UIApplication.shared.open(url)
+            }
+        }
     }
-    */
 
 }
